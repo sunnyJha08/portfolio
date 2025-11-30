8cb8679ac73f183c037355ee0a173cbd30afae5b
sunnyJha08 <sunnyjha98971@gmail.com>
2025-11-30 23:48:48 +0530

    feat: replace placeholder content with TypographyH1 component in Home

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-
+import { TypographyH1 } from "~/components/ui/typography";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
@@ -10,13 +10,19 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center">
-      <button>Click me</button>
-      <h1 className="text-5xl">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
-        voluptatem, error necessitatibus sed a officiis ipsa natus! Ab
-        reiciendis aut neque magni ipsum repudiandae quas, aliquid, velit
-        tempore placeat eius.
-      </h1>
+      <TypographyH1 />
+      {/* <p className="text-muted-foreground text-xl">
+        A modal dialog that interrupts the user with important content and
+        expects a response.
+      </p>
+      <p className="leading-7 not-first:mt-6">
+        The king, seeing how much happier his subjects were, realized the error
+        of his ways and repealed the joke tax.
+      </p>
+      <blockquote className="mt-6 border-l-2 pl-6 italic">
+        &quot;After all,&quot; he said, &quot;everyone enjoys a good joke, so
+        it&apos;s only fair that they should pay for the privilege.&quot;
+      </blockquote> */}
     </div>
   );
 }
