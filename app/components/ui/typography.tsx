e1e932eb67267844b337b448b6d60610e0f414bb
sunnyJha08 <sunnyjha98971@gmail.com>
2025-11-30 23:48:41 +0530

    feat: add typography components for headings, paragraphs, and blockquotes

---
+++ app/components/ui/typography.tsx
@@ -0,0 +1,65 @@
+export function TypographyH1() {
+  return (
+    <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
+      Sunny Jha: A Full Stack Developer
+    </h1>
+  );
+}
+export function TypographyH2() {
+  return (
+    <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+      The People of the Kingdom
+    </h2>
+  );
+}
+export function TypographyH3() {
+  return (
+    <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+      The Joke Tax
+    </h3>
+  );
+}
+export function TypographyH4() {
+  return (
+    <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+      People stopped telling jokes
+    </h4>
+  );
+}
+export function TypographyP() {
+  return (
+    <p className="leading-7 not-first:mt-6">
+      The king, seeing how much happier his subjects were, realized the error of
+      his ways and repealed the joke tax.
+    </p>
+  );
+}
+export function TypographyBlockquote() {
+  return (
+    <blockquote className="mt-6 border-l-2 pl-6 italic">
+      &quot;After all,&quot; he said, &quot;everyone enjoys a good joke, so
+      it&apos;s only fair that they should pay for the privilege.&quot;
+    </blockquote>
+  );
+}
+export function TypographyLead() {
+  return (
+    <p className="text-muted-foreground text-xl">
+      A modal dialog that interrupts the user with important content and expects
+      a response.
+    </p>
+  );
+}
+export function TypographyLarge() {
+  return <div className="text-lg font-semibold">Are you absolutely sure?</div>;
+}
+export function TypographySmall() {
+  return (
+    <small className="text-sm leading-none font-medium">Email address</small>
+  );
+}
+export function TypographyMuted() {
+  return (
+    <p className="text-muted-foreground text-sm">Enter your email address.</p>
+  );
+}
--- app/components/ui/typographyh1.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Typographyh1() {
-  return <div>typographyh1</div>;
-}
