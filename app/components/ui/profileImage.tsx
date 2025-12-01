389a89b6dab5a2f438de9bb3e28fd6788746eb53
sunnyJha08 <sunnyjha98971@gmail.com>
2025-12-01 22:13:43 +0530

    feat: add ProfileImage component for displaying user profile images

---
+++ app/components/ui/profileImage.tsx
@@ -0,0 +1,11 @@
+import profileImg from "../../assets/images/profilePic.jpg";
+export default function ProfileImage(props: { className?: string }) {
+  return (
+    <img
+      src={profileImg}
+      alt="Profile image"
+      loading="lazy"
+      className={`object-cover ${props.className}`}
+    />
+  );
+}
