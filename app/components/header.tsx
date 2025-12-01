6fb37be501a5db25a6c3dc8f1ecc78d31cba0daa
sunnyJha08 <sunnyjha98971@gmail.com>
2025-12-01 22:12:50 +0530

    feat: replace image logo with ProfileImage component for improved modularity

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,21 +1,34 @@
 import { NavLink } from "react-router";
-import pp from "../assets/images/profilePic.jpg";
+import ProfileImage from "./ui/profileImage";
 export default function Header() {
   return (
     <header className="sticky top-0 z-20 mx-auto flex w-screen max-w-3xl items-center justify-between p-4 shadow-md backdrop-blur-sm">
       <NavLink to={"/"}>
-        <img
-          src={pp}
-          alt="Logo"
-          className="size-12 rounded-sm border border-neutral-400 object-cover shadow-xl transition-all hover:scale-90"
-          loading="lazy"
+        <ProfileImage
+          className={
+            "size-12 rounded-sm border shadow-xl transition-all hover:scale-90"
+          }
         />
       </NavLink>
-      <nav className="flex items-center gap-4 text-xl font-medium tracking-tight text-neutral-400">
-        <NavLink to={"/blogs"} className={"hover:underline"}>
+      <nav className="flex items-center gap-4 text-xl font-medium tracking-tight dark:text-white">
+        <NavLink
+          to={"/blogs"}
+          className={({ isActive }) =>
+            isActive
+              ? "text-neutral-400"
+              : "text-black hover:underline dark:text-white"
+          }
+        >
           Blogs
         </NavLink>
-        <NavLink to={"/contact"} className={"hover:underline"}>
+        <NavLink
+          to={"/contact"}
+          className={({ isActive }) =>
+            isActive
+              ? "text-neutral-400"
+              : "text-black hover:underline dark:text-white"
+          }
+        >
           Contact
         </NavLink>
       </nav>
