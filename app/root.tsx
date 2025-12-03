cf1bfcc99822ffac28f4c38628d8e381211a5b09
sunnyJha08 <sunnyjha98971@gmail.com>
2025-12-03 21:31:53 +0530

    fix: adjust padding in main layout for improved spacing

--- app/root.tsx
+++ app/root.tsx
@@ -46,7 +46,7 @@ export default function App() {
   return (
     <>
       <Header />
-      <main className="mx-auto max-w-3xl px-4 py-16">
+      <main className="mx-auto max-w-3xl px-4 py-4">
         <Outlet />
       </main>
     </>
