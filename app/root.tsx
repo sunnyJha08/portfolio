0cf5779d086ae6c0121399828c5ad5dafaceffb8
sunnyJha08 <sunnyjha98971@gmail.com>
2025-12-12 23:58:33 +0530

    feat: added footer in the root.tsx so that it will rendered in entire the website just like the header navbar

--- app/root.tsx
+++ app/root.tsx
@@ -10,6 +10,7 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 import Header from "./components/header";
+import Footer from "./components/footer";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -49,6 +50,7 @@ export default function App() {
       <main className="mx-auto max-w-3xl px-4 py-4">
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
