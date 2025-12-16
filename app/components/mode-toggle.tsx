f89e67c487b318cdf881bacdf03c853f7fbab845
sunnyJha08 <sunnyjha98971@gmail.com>
2025-12-16 22:08:18 +0530

    feat: mode toggle component for handling context of theme using a button.

---
+++ app/components/mode-toggle.tsx
@@ -0,0 +1,16 @@
+import { Moon, Sun } from "lucide-react";
+import { Button } from "./ui/button";
+import { useTheme } from "./theme-provider";
+
+export function ModeToggle() {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <Button
+      onClick={toggleTheme}
+      className="absolute right-0 bottom-0 size-8 cursor-pointer rounded-full"
+    >
+      {theme === "dark" ? <Sun /> : <Moon />}
+    </Button>
+  );
+}
