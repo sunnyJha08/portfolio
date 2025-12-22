641f0a8f8c48adfc98570815ce3cdebe5b50e5e6
sunnyJha08 <sunnyjha98971@gmail.com>
2025-12-22 23:45:51 +0530

    fix: refactor contact page and removed previous component

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -1,12 +1,22 @@
 import { Send } from "lucide-react";
 import { Button } from "~/components/ui/button";
-import { InputWithLabel, TextareaWithLabel } from "~/components/ui/input";
+import {
+  Input,
+  InputWithLabel,
+  TextareaWithLabel,
+} from "~/components/ui/input";
 import {
   TypographyH1,
   TypographyH4,
   TypographyLead,
+  TypographyMuted,
+  TypographyP,
+  TypographySmall,
 } from "~/components/ui/typography";
 import type { Route } from "./+types/contact";
+import { Form } from "react-router";
+import { Label } from "~/components/ui/label";
+import { Textarea } from "~/components/ui/textarea";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -15,51 +25,67 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Contact() {
-  const handleSubmit = (e: React.FormEvent) => {
-    console.log(e);
-    e.preventDefault();
-  };
-
+const handleSubmit = (e: React.FormEvent) => {
+  console.log(e);
+  e.preventDefault();
+};
+export function ContactForm() {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <Form
+      method="post"
+      className="border-border bg-card flex w-full flex-col gap-4 rounded-lg p-4 shadow-sm"
+      onSubmit={handleSubmit}
+    >
+      <div className="space-y-2">
+        <Label htmlFor="name">Name *</Label>
+        <Input type="text" required placeholder="Your name" />
+        <TypographySmall
+          value="Message must be at least 10 characters."
+          className="text-destructive hidden"
+        />
+      </div>
+
+      <div className="space-y-2">
+        <Label htmlFor="email">Email *</Label>
+        <Input type="email" required placeholder="sunnyjha98971@gmail.com" />
+        <TypographySmall
+          value="Message must be at least 10 characters."
+          className="text-destructive hidden"
+        />
+      </div>
+
+      <div className="space-y-2">
+        <Label>Your message *</Label>
+        <Textarea className="h-30" />
+        <TypographySmall
+          value="Message must be at least 10 characters."
+          className="text-destructive hidden"
+        />
+      </div>
+      <Button
+        type="submit"
+        className="cursor-pointer"
+        aria-label="Send message button"
+      >
+        <Send />
+        Send Message
+      </Button>
+    </Form>
+  );
+}
+export default function Contact() {
+  return (
+    <div className="mx-auto flex max-w-lg flex-col items-center justify-center">
       <TypographyH1 value="Contact me" />
       <TypographyLead
         value="Drop in your info here and, I will get back to you."
-        className="mt-2 max-w-lg text-center text-neutral-400"
+        className="my-2 max-w-lg text-center text-neutral-400"
       />
-
-      <form
-        action="# Specifies the URL where the form data will be sent for processing when the form is submitted. This URL typically points to a server-side script or program"
-        method="post"
-        className="border-border bg-card mt-6 flex w-full max-w-lg flex-col gap-4 rounded-lg p-4 shadow-sm"
-        onSubmit={handleSubmit}
-      >
+      <div className="my-6 w-full space-y-2 text-left">
         <TypographyH4 value="Send me a message" />
-        <InputWithLabel
-          htmlForAndId={"senderName"}
-          label={"Name *"}
-          placeholder={"Your Name"}
-          type="text"
-          required={true}
-        />
-        <InputWithLabel
-          htmlForAndId={"email"}
-          label={"Email *"}
-          placeholder={"sunnyjha98971@gmail.com"}
-          type={"email"}
-          required={true}
-        />
-        <TextareaWithLabel />
-        <Button
-          type="submit"
-          className="cursor-pointer"
-          aria-label="Send message button"
-        >
-          <Send />
-          Send Message
-        </Button>
-      </form>
+        <TypographyMuted value="Fill out the form below and I will get back to you as soon as possible." />
+      </div>
+      <ContactForm />
     </div>
   );
 }
