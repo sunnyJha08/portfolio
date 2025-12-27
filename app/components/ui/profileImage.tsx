2127c14759bb2f3016ca4d3cfc73ec22ddd92eb6
sunnyJha08 <sunnyjha98971@gmail.com>
2025-12-27 23:38:16 +0530

    fix: changed image format to AVIF and fixed the import of the profileImage while removing the old jpg image.

--- app/components/ui/profileImage.tsx
+++ app/components/ui/profileImage.tsx
@@ -1,7 +1,7 @@
 export function ProfileImage(props: { className?: string }) {
   return (
     <img
-      src="/profilePic.jpg"
+      src="/profilePic.avif"
       alt="Profile image"
       loading="lazy"
       className={`object-cover ${props.className}`}
