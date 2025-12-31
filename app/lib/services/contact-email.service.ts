ee286204164da5bc48f0da10bb43c09e04466131
sunnyJha08 <sunnyjha98971@gmail.com>
2025-12-31 23:36:22 +0530

    feat: Abstracted email service from the main component to a different file.

--- app/lib/services/contact-email.service.ts
+++ app/lib/services/contact-email.service.ts
@@ -1,12 +1,16 @@
 import { Resend } from "resend";
 
-export async function sendContactEmail(
-  name: string,
-  email: string,
-  message: string,
-) {
+export async function sendContactEmail({
+  name,
+  email,
+  message,
+}: {
+  name: string;
+  email: string;
+  message: string;
+}) {
   if (!process.env.RESEND_EMAIL_API_KEY) {
-    throw new Error("Missing RESEND_EMAIL_API_KEY");
+    throw new Error("Missing EMAIL_SERVICE_API_KEY");
   }
 
   const resend = new Resend(process.env.RESEND_EMAIL_API_KEY);
@@ -14,7 +18,7 @@ export async function sendContactEmail(
   return resend.emails.send({
     from: "Portfolio <onboarding@resend.dev>",
     to: ["sunnyjha98@gmail.com"],
-    subject: "Contact Form Submission",
+    subject: "Portfolio Form Submission",
     html: `
       <h3>New Contact Message</h3>
       <p><strong>Name:</strong> ${name}</p>
